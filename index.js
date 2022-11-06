646fa4cdeaad670e593a032f52b4cfb69cd98540
grinkevichvm <googlvint@gmail.com>
2022-11-06 13:27:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@
 //-----------------------------------------------------------------------------
 
 exports.helloWorld = (req, res) => {
-  const message="<font color='blue'>СloudFunction of GVM ! PROD !!!!!</font><br><b>App Version 1.1</b>";
+  const message="<font color='blue'>СloudFunction of GVM ! PROD !!!!!</font><br><b>App Version 1000.1</b>";
   res.status(200).send(message);
 };
